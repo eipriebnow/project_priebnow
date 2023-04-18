273837794b1660aebdc614f0ed2ab9decefc26b4
eipriebnow <99771462+eipriebnow@users.noreply.github.com>
2023-04-17 21:13:29 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -72,7 +72,6 @@ console.log(potencia(6));
 
 !!!!!!!!!!!!!AULA 2!!!!!!!!!!!!!
 EXERCÍCIO 1: Faça um formulário que receba informações do usuário na página web
-*/
 
 const enviar = document.querySelector("#enter");
 
@@ -86,3 +85,70 @@ enviar.addEventListener('click', function(form){
     console.log(`Nome: ${nome}`);
 
 });
+
+const enviar = document.querySelector("#enter");
+
+enviar.addEventListener("click", function(form){
+
+        form.preventDefault();
+
+    const txnome = document.querySelector("#txnome");
+    const nome = txnome.value;
+
+    console.log(`Nome: ${nome}`);
+
+    const txemail = document.querySelector("#txemail");
+    const email = txemail.value;
+
+    console.log(`Email: ${email}`);
+
+    const txtelefone = document.querySelector("#txtelefone");
+    const telefone = txtelefone.value;
+
+    console.log(`Telefone: ${telefone}`);
+
+    const txendereco = document.querySelector("#txendereco");
+    const endereco = txendereco.value;
+
+    console.log(`Endereço: ${endereco}`);
+
+    const txmensagem = document.querySelector("#txmensagem");
+    const mensagem = txmensagem.value;
+
+    console.log(`Mensagem: ${mensagem}`);
+
+
+});
+
+AULA 3!!!!!!!!!!!!!!!
+EXERCICIO 1
+
+const pessoa = {
+    nome: 'Jorge Silva',
+    dtNascimento: 1995,
+
+};
+function mostrar (nome, dtNascimento){
+    console.log(`${this.nome} nasceu em ${this.dtNascimento}`);
+};
+
+mostrar.call(pessoa);
+*/
+
+const carro = {
+    marca: "Ford",
+    cor: "preto",
+    placa: "SVG 2267",
+    velocidade_atual: 50,
+    marcha_atual: "Quarta",
+    freio_de_mao_puxado: true,
+    chave_virada: true
+
+
+};
+function mostrar(marca, cor, placa, velocidade_atual,marcha_atual, freio_de_mao_puxado, chave_virada) {
+    console.log(`${this.marca} da cor ${this.cor} de placa ${this.placa} de velocidade atual ${this.velocidade_atual} na marcha ${this.marcha_atual} com o freio de mão ${this.freio_de_mao_puxado} e a chave virada ${this.chave_virada}`);
+
+};
+
+mostrar.call(carro);
